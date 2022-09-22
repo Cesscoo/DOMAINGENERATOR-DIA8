3bd1f531b374e734be5672bf7b3c18e9afe5ee52
Cesscoo <martinhernandez.fj@gmail.com>
2022-09-22 08:34:06 +0000

    Aparecen todas los dominios posibles

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#dominio").innerHTML = dominions();
+  console.log("Hemos conseguido imprimir en pantalla todas las opciones !");
 };
+
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var fin = [".com", ".es", ".net"];
+
+let resultado = [];
+
+function dominions() {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let y = 0; y < noun.length; y++) {
+        for (let z = 0; z < fin.length; z++)
+          resultado.push(pronoun[i] + adj[x] + noun[y] + fin[z]);
+      }
+    }
+  }
+  return resultado;
+}
